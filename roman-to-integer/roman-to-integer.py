27efb0646ab4c60cd0997167963d6105f8b34e0f
jinimong <phoongdeng@gmail.com>
2021-08-28 03:15:30 +0900

    Time: 65 ms (9.90%), Space: 14.1 MB (83.96%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,32 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {
+            "M": 1000,
+            "CM": 900,
+            "D": 500,
+            "CD": 400,
+            "C": 100,
+            "XC": 90,
+            "L": 50,
+            "XL": 40,
+            "X": 10,
+            "IX": 9,
+            "V": 5,
+            "IV": 4,
+            "I": 1,
+        }
+        result = 0
+        i = 0
+        while i < len(s):
+            if i < len(s) - 1:
+                num = d.get(s[i : i + 2])
+                if num:
+                    result += num
+                    i += 2
+                    continue
+            num = d.get(s[i])
+            if num:
+                result += num
+                i += 1
+
+        return result
